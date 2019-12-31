dfbf8a37747aa2a0a0c1561f7ec87a3bc7497042
Ryan Kadel <ryan.kadel88@gmail.com>
2019-12-31 05:16:57 -0500

    commit for deployment

--- server.js
+++ server.js
@@ -48,9 +48,19 @@ function handleGetMovie(req, res) {
   res.json(response)
 }
 
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
 app.get('/movie', handleGetMovie)
 
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
